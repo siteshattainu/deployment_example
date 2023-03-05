c1e47c0f775642d6a4932bfe7d1b2f38b3b12cb1
siteshattainu <122366313+siteshattainu@users.noreply.github.com>
2023-03-05 11:15:23 +0530

    Add files via upload

--- database.js
+++ database.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 
-mongoose.connect("mongodb://localhost:27017/backend_project", {useNewUrlParser: true, useUnifiedTopology: true}, 
+mongoose.connect("mongodb+srv://sitesh:12345@cluster0.juo8kym.mongodb.net/backend_project?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true}, 
     (err) => {
         if(err){
             console.log("Connection Failed", err)
